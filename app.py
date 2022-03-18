767f763ddcddb0734bb662456a18afe85593e3a9
zhibin.an <zhibin.an@castbox.fm>
2022-03-18 10:58:22 +0800

    refactor

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from flask import Flask, request, jsonify
 from model import BookModel
-from db import db_create_book, db_list_books, init_seq_id_init, seq_id_atomic_inc, db_retrieve_book, db_update_book, \
+from db import db_create_book, db_list_books, db_retrieve_book, db_update_book, \
     db_delete_book
-from marshmallow import INCLUDE, EXCLUDE
 
 app = Flask(__name__)
-
-# init db
-BOOK_SEQ_NAME = 'book'
-init_seq_id_init(BOOK_SEQ_NAME, 1)
 
 
 @app.route("/create", methods=["POST"])
@@ -17,16 +12,14 @@ def create_book():
     Create a new book
     """
     payload = request.get_json()
+    if "id" in payload:    # user cannot pass id when creating a new book
+        payload.pop("id")
     status = BookModel.Schema().validate(payload, partial=("id",))   # no validation to id
     if status:
         return jsonify(status), 400
-    payload["id"] = seq_id_atomic_inc(BOOK_SEQ_NAME)
-    book = BookModel(**payload)
-    success = db_create_book(book)
-    if not success:
-        return jsonify({"message": "Book already exists"}), 409
-    book_db = db_retrieve_book(book.id)
-    return jsonify(data=book_db.to_dict()), 201
+    book = BookModel.from_dict(payload)
+    db_create_book(book)
+    return jsonify(data=book.to_dict()), 201
 
 
 @app.route("/list", methods=["GET"])
@@ -35,7 +28,8 @@ def list_book():
     Retrieve all books
     """
     books = db_list_books()
-    return jsonify(data=[book.to_dict() for book in books]), 200
+    res = {"list": [book.to_dict() for book in books], "count": len(books)}
+    return jsonify(data=res), 200
 
 
 @app.route("/retrieve", methods=["GET"])
@@ -44,7 +38,7 @@ def retrieve_book():
     Retrieve a book
     """
     book_id = request.args.get("id")
-    book = db_retrieve_book(int(book_id))
+    book = db_retrieve_book(book_id)
     if book:
         return jsonify(data=book.to_dict()), 200
     return jsonify({"message": "Book not found"}), 404
@@ -68,15 +62,14 @@ def update_book():
     return jsonify(data=book_db.to_dict()), 200
 
 
-@app.route("/delete", methods=["POST"])
+@app.route("/delete", methods=["DELETE"])
 def delete_book():
     """"
     Delete a book
     """
-    payload = request.get_json()
-    book_id = payload.get("id")
+    book_id = request.args.get("id")
     if not book_id:
-        return jsonify({"message": "Book id not found"}), 400
+        return jsonify({"message": "Book id is required"}), 400
     success = db_delete_book(book_id)
     if not success:
         return jsonify({"message": "Book Delete failed"}), 404
--- db.py
+++ db.py
@@ -5,43 +5,26 @@ from typing import List, Dict, Any
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 TDb = client["data"]
 TBook = TDb['book']
-TCounter = TDb['seq_id']
-
-
-def init_seq_id_init(name: str, v: int):
-    try:
-        TCounter.insert_one({"_id": name, "v": v})
-    except pymongo.errors.DuplicateKeyError:
-        pass
-
-
-def seq_id_atomic_inc(name, inc=1) -> int:
-    TCounter.update_one({"_id": name}, {"$inc": {"v": inc}})
-    return TCounter.find_one({"_id": name})['v']
 
 
 def db_create_book(book: BookModel) -> bool:
-    try:
-        TBook.insert_one(book.__dict__)
-        return True
-    except pymongo.errors.DuplicateKeyError:
-        return False
+    TBook.insert_one(book.__dict__)
 
 
-def db_update_book(book_id: int, book: Dict[str, Any]) -> bool:
+def db_update_book(book_id: str, book: Dict[str, Any]) -> bool:
     res = TBook.update_one({"id": book_id}, {"$set": book})
     return res.modified_count > 0
 
 
 def db_list_books() -> List[BookModel]:
-    return [BookModel(**r) for r in TBook.find()]
+    return [BookModel.from_dict(r) for r in TBook.find()]
 
 
-def db_retrieve_book(book_id: int) -> BookModel:
+def db_retrieve_book(book_id: str) -> BookModel:
     _book = TBook.find_one({"id": book_id})
-    return BookModel(**_book) if _book else None
+    return BookModel.from_dict(_book) if _book else None
 
 
-def db_delete_book(book_id: int) -> bool:
+def db_delete_book(book_id: str) -> bool:
     res = TBook.delete_one({"id": book_id})
     return res.deleted_count > 0
